92752dd65e332fa3cdd5dfd18ee4e084f15e34a3
Travis Dahlke <travis.dahlke@tstmedia.com>
2013-01-29 16:31:16 -0600

    Update spec to support Mongoid 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,17 @@ require 'bundler/setup'
 
 require 'mongoid'
 require 'mongoid/mongoid_retry'
+require 'database_cleaner'
 
 require 'rspec'
 
 Mongoid.configure do |config|
-  config.master = Mongo::Connection.new.db('mongoid_retry_test')
-  config.allow_dynamic_fields = false
-  config.persist_in_safe_mode = true
+  if Mongoid::VERSION >= "3"
+    config.connect_to('mongoid_retry_test')
+  else
+    config.master = Mongo::Connection.new.db('mongoid_retry_test')
+    config.allow_dynamic_fields = false
+  end
 end
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
@@ -17,6 +21,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 RSpec.configure do |config|
   config.mock_with :rspec
   config.after :each do
-    Mongoid.master.collections.reject { |c| c.name =~ /^system\./ }.each(&:remove)
+    DatabaseCleaner.clean
   end
 end
