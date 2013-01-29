adff37bf2929824265c064e84945c8ac50759fbf
Travis Dahlke <travis.dahlke@tstmedia.com>
2013-01-29 16:31:57 -0600

    Update mongoid_retry to support Mongoid 3

--- lib/mongoid/mongoid_retry.rb
+++ lib/mongoid/mongoid_retry.rb
@@ -5,34 +5,64 @@ module Mongoid
 
     DUPLICATE_KEY_ERROR_CODES = [11000,11001]
 
+    if Mongoid::VERSION < '3'
+      def self.error_message(exception)
+        exception.message
+      end
+
+      def self.error_code(exception)
+        exception.error_code
+      end
+    else
+      def self.error_message(exception)
+        exception.details["err"]
+      end
+
+      def self.error_code(exception)
+        exception.details["code"]
+      end
+    end
+
+    def self.is_a_duplicate_key_error?(exception)
+      DUPLICATE_KEY_ERROR_CODES.include?(error_code(exception))
+    end
+
     # Catch a duplicate key error
     def save_and_retry
-      begin
-        save!
-      rescue Mongo::OperationFailure => e
-        if is_a_duplicate_key_error?(e)
-          keys = duplicate_key(e)
-          if (duplicate = find_duplicate(keys))
-            update_document!(duplicate)
-          end
-        else
-          raise e
+      if ::Mongoid::VERSION < '3'
+        begin
+          save!
+        rescue Mongo::OperationFailure => e
+          retry_if_duplicate_key_error(e)
         end
+      else
+        begin
+          with(safe: true).save!
+        rescue Moped::Errors::OperationFailure => e
+          retry_if_duplicate_key_error(e)
+        end
+      end
+    end
+
+    def retry_if_duplicate_key_error(e)
+      if ::Mongoid::MongoidRetry.is_a_duplicate_key_error?(e)
+        keys = duplicate_key(e)
+        if (duplicate = find_duplicate(keys))
+          update_document!(duplicate)
+        end
+      else
+        raise e
       end
     end
 
     private
 
-    def is_a_duplicate_key_error?(exception)
-      DUPLICATE_KEY_ERROR_CODES.include?(exception.error_code)
-    end
-
     def find_duplicate(keys)
-      self.class.first(conditions: keys)
+      self.class.where(keys).first
     end
 
     def duplicate_key(exception)
-      index = exception.message[/(?<=\.\$)\w*/,0]
+      index = ::Mongoid::MongoidRetry.error_message(exception)[/(?<=\.\$)\w*/,0]
       fields = index.split(/\d/).map{|s| s.gsub(/^_|_-?$/,'')}
       fields.inject({}) {|hash, key| hash[key] = send(key) if respond_to?(key); hash}
     end
